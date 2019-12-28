d14847738ff53d4cecb30e4e4bf63e4dfad3741d
Alex Palko <43597659+alexpalko@users.noreply.github.com>
2019-12-28 15:04:55 +0200

    Added farm prefabs

---
+++ Assets/ARTowerDefense/Scripts/Misc/Chicken.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    private Animator m_Animator;
+    private float m_Cooldown;
+    private const float k_CooldownDefault = 5;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (m_Cooldown >= 0)
+        {
+            m_Cooldown -= Time.deltaTime;
+            return;
+        }
+        var chance = Random.value;
+        if (chance < .15f)
+        {
+            m_Animator.SetBool("Eat", !m_Animator.GetBool("Eat"));
+            m_Cooldown = k_CooldownDefault;
+        }
+        else if (chance < .3f)
+        {
+            m_Animator.SetBool("Turn Head", !m_Animator.GetBool("Turn Head"));
+            m_Cooldown = k_CooldownDefault;
+        }
+    }
+}
